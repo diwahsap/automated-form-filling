ffeb02b001d9ff89f8a1e9e0cf7a77f17d77419e
Dimas Wahyu Saputro <73456370+dimaswsss@users.noreply.github.com>
2023-01-03 17:45:35 +0700

    update

--- main.py
+++ main.py
@@ -11,20 +11,33 @@ driver.implicitly_wait(10)
 driver.find_element_by_css_selector('#modal_warning > div > div > div.modal-header > button').click()
 
 # authentication
-driver.find_element_by_css_selector('#user').send_keys('youremail') # your email
-driver.find_element_by_css_selector('#pwd').send_keys('yourpassword') # your password
+driver.find_element_by_css_selector('#user').send_keys('dimas.120450081@student.itera.ac.id') # your email
+driver.find_element_by_css_selector('#pwd').send_keys('Dimas.120450081') # your password
 
 # verification
 messagebox.showinfo(title='Verification', message='Finish on screen verification, and then click OK.')
 driver.find_element_by_css_selector('#vanilla-slideshow-container > div.login-page > div > div > div > div.col-lg-4.col-md-4.col-md-offset-1.col-sm-5 > div > div.form-body > form > button').click()
 
+# Wait for the page to load
+driver.implicitly_wait(60)
+
+# close pengumuman
+driver.find_element_by_css_selector('#modal_kue > div > div > div.modal-header > button').click()
+
 # click tab Kuesioner
 driver.find_element_by_css_selector('body > div.container.content > div > div.col-md-3 > div:nth-child(2) > a:nth-child(6)').click()
 
-# Kuesioner semester 2022/2023 Ganjil -> ada 5 bagian. Isinya 7 pertanyaan.
-for i in range(1, 6):
-    driver.find_element_by_css_selector(f'body > div.container.content > div > div.col-md-9 > div > div.box-body > table:nth-child(7) > tbody > tr:nth-child({i}) > td:nth-child(6) > a').click()
+# Kuesioner semester 2022/2023 Ganjil -> ada 5 bagian. Isinya 1@7, 4@3 pertanyaan.
+for i in range(1, 2):
+    driver.find_element_by_css_selector(f'body > div.container.content > div > div.col-md-9 > div > div.box-body > table:nth-child(3) > tbody > tr:nth-child({i}) > td:nth-child(5) > a').click()
     for i in range(1, 8):
+        driver.find_element_by_css_selector(f'body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div:nth-child({i}) > div > label:nth-child(2) > input[type=radio]').click()
+    driver.find_element_by_css_selector('body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div.box-footer > button').click()
+    driver.find_element_by_css_selector('body > div.container.content > div > div.col-md-9 > div.alert.alert-info > button').click()
+
+for i in range(2, 6):
+    driver.find_element_by_css_selector(f'body > div.container.content > div > div.col-md-9 > div > div.box-body > table:nth-child(3) > tbody > tr:nth-child({i}) > td:nth-child(5) > a').click()
+    for i in range(1, 4):
         driver.find_element_by_css_selector(f'body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div:nth-child({i}) > div > label:nth-child(2) > input[type=radio]').click()
     driver.find_element_by_css_selector('body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div.box-footer > button').click()
     driver.find_element_by_css_selector('body > div.container.content > div > div.col-md-9 > div.alert.alert-info > button').click()
