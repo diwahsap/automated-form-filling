2a8940ff952eab5659aa463051986570c87f8d55
Dimas Wahyu Saputro <73456370+dimaswsss@users.noreply.github.com>
2023-01-03 12:09:05 +0700

    update main.py

--- main.py
+++ main.py
@@ -21,14 +21,24 @@ driver.find_element_by_css_selector('#vanilla-slideshow-container > div.login-pa
 # click tab Kuesioner
 driver.find_element_by_css_selector('body > div.container.content > div > div.col-md-3 > div:nth-child(2) > a:nth-child(6)').click()
 
-for i in range(1, 20):
+# Kuesioner semester 2022/2023 Ganjil -> ada 5 bagian. Isinya 7 pertanyaan.
+for i in range(1, 6):
+    driver.find_element_by_css_selector(f'body > div.container.content > div > div.col-md-9 > div > div.box-body > table:nth-child(7) > tbody > tr:nth-child({i}) > td:nth-child(6) > a').click()
+    for i in range(1, 8):
+        driver.find_element_by_css_selector(f'body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div:nth-child({i}) > div > label:nth-child(2) > input[type=radio]').click()
+    driver.find_element_by_css_selector('body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div.box-footer > button').click()
+    driver.find_element_by_css_selector('body > div.container.content > div > div.col-md-9 > div.alert.alert-info > button').click()
+
+# part 1 -> kuesioner kepuasan mahasiswa terhadap dosen itera. Isinya ada 13 pertanyaan
+for i in range(1, 11):
     driver.find_element_by_css_selector(f'body > div.container.content > div > div.col-md-9 > div > div.box-body > table:nth-child(7) > tbody > tr:nth-child({i}) > td:nth-child(6) > a').click()
     for i in range(1, 14):
         driver.find_element_by_css_selector(f'body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div:nth-child({i}) > div > label:nth-child(2) > input[type=radio]').click()
     driver.find_element_by_css_selector('body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div.box-footer > button').click()
     driver.find_element_by_css_selector('body > div.container.content > div > div.col-md-9 > div.alert.alert-info > button').click()
 
-for i in range(20, 39):
+# part 2 -> kuesioner evaluasi perkuliahan. Isinya ada 11 pertanyaan.
+for i in range(11, 21):
     driver.find_element_by_css_selector(f'body > div.container.content > div > div.col-md-9 > div > div.box-body > table:nth-child(7) > tbody > tr:nth-child({i}) > td:nth-child(6) > a').click()
     for i in range(1, 12):
         driver.find_element_by_css_selector(f'body > div.container.content > div.row > div.col-md-9 > div > div.box-body > form > div > div:nth-child({i}) > div > label:nth-child(2) > input[type=radio]').click()
